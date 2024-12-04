85a22107a43227071439cd7ce85f560cde9a6c21
adrianocruzweb <adrianocruzweb@hotmail.com>
2024-12-04 13:17:30 -0400

    backtotop

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -51,3 +51,21 @@ menuLinks.forEach(menu => {
         menu.classList.add("active");
     })
 });
+
+const backToTopButton = document.getElementById("backToTop");
+window.onscroll = function() {
+
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
+
+backToTopButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior:
+        'smooth'
+    });
+});
